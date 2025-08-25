0eaf0c5c36db2cb8fcca7b300eb2f9f220bc6621
zhanghe5 <zhanghe5@lixiang.com>
2025-08-25 13:58:31 +0800

    deekseek接入

---
+++ springAILearn/quick-start/src/test/java/com/example/quickstart/TestDeepSeek.java
@@ -0,0 +1,18 @@
+package com.example.quickstart;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.ai.deepseek.DeepSeekChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TestDeepSeek {
+
+    @Test
+    public void testDeepSeek(@Autowired DeepSeekChatModel deepSeekChatModel) {
+        String content = deepSeekChatModel.call("你好，你是谁");
+        System.out.println(content);
+    }
+
+}
