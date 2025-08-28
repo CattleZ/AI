5bc1dfdd6aed09e1df0469308acdd4b410de42b7
zhanghe5 <zhanghe5@lixiang.com>
2025-08-28 18:05:58 +0800

    提示词模板

--- springAILearn/chatClineTest/src/test/java/com/example/chatclinetest/TestPrompt.java
+++ springAILearn/chatClineTest/src/test/java/com/example/chatclinetest/TestPrompt.java
@@ -3,16 +3,32 @@ package com.example.chatclinetest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.ai.template.st.StTemplateRenderer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import reactor.core.publisher.Flux;
 
+import java.util.Map;
+
 @SpringBootTest
 public class TestPrompt {
 
+
+
     @Test
     public void testPrompt(@Autowired ChatClient.Builder builder) {
+        // 提示词模板
+        PromptTemplate promptTemplate = PromptTemplate.builder()
+                .renderer(StTemplateRenderer.builder().startDelimiterToken('<').endDelimiterToken('>').build())
+                .template("""
+                        告诉我5部<composer>电影
+                        """).build();
+
+        String prompt = promptTemplate.render(Map.of("composer","John Williams"));
+
+        // 系统提示词
         ChatClient chatClient = builder.defaultSystem("""
                 # 角色说明
                 你是一名专业的法律顾问AI....
@@ -29,7 +45,9 @@ public class TestPrompt {
                 当前服务的用户 姓名{name}, 性别：{sex}
                 """).build();
 
-        Flux<String> content = chatClient.prompt().system(p -> p.param("name", "张三").param("sex", "男")).user("你好").stream().content();
+        Flux<String> content = chatClient.prompt().system(p -> p.param("name", "张三").param("sex", "男"))
+                // 用户提示词
+                .user("你好").stream().content();
         content.toIterable().forEach(System.out::println);
     }
 }
