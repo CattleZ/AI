6bcf8fadb78b4e5a33f16f06b70f2f9140957c3b
zhanghe5 <zhanghe5@lixiang.com>
2025-08-29 14:45:35 +0800

    敏感日志拦截器

--- springAILearn/chatClineTest/src/test/java/com/example/chatclinetest/TestAdvisor.java
+++ springAILearn/chatClineTest/src/test/java/com/example/chatclinetest/TestAdvisor.java
@@ -3,9 +3,12 @@ package com.example.chatclinetest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SafeGuardAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 public class TestAdvisor {
@@ -16,6 +19,20 @@ public class TestAdvisor {
         ChatClient chatClient = chatClientBuilder
                 .defaultAdvisors(new SimpleLoggerAdvisor()).build();
         String content = chatClient.prompt().user("你好").call().content();
+        System.out.println( content);
+    }
+
+
+    /**
+     * 敏感词拦截
+     * @param chatClientBuilder
+     */
+    @Test
+    public void testSensitiveAdvisor(@Autowired ChatClient.Builder chatClientBuilder)
+    {
+        ChatClient chatClient = chatClientBuilder
+                .defaultAdvisors(new SimpleLoggerAdvisor(), new SafeGuardAdvisor(List.of("sb"))).build();
+        String content = chatClient.prompt().user("你好,gorge,sb").call().content();
         System.out.println( content);
     }
 }
